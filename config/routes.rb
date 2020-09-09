6ba2ea6f6e1e5273cadb5049b33f793cce608dcf
Denis Naughton <mjolinor75@hotmail.com>
2020-09-09 19:55:51 -0400

    changed layout with rebase, corrected layout files

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   
   
   
-<<<<<<< HEAD
-  get 'research/research_home'
-  get 'research/lesson_1'
-  get 'research/lesson_2'
-=======
->>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
+# <<<<<<< HEAD
+  # get 'research/research_home'
+  # get 'research/lesson_1'
+  # get 'research/lesson_2'
+# =======
+# >>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
 #home section  
   root  'static_pages#home'	
   get 'intro' => 'static_pages#intro'
@@ -27,12 +27,12 @@ Rails.application.routes.draw do
   get 'history_lesson_2' => 'ap_history#lesson_2'
 
   #ap research
-<<<<<<< HEAD
+# <<<<<<< HEAD
   get 'research_home' => 'research#research_home'
   get 'research_lesson_1' => 'research#lesson_1'
   get 'research_lesson_2' => 'research#lesson_1'
-=======
->>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
+# =======
+# >>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
   
   get 'research_home' => 'research#research_home'
   get 'research_lesson_1' => 'research#lesson_1'
@@ -45,14 +45,14 @@ Rails.application.routes.draw do
   get 'english_lesson_2' => 'ap_english#lesson_2'
  
   
-<<<<<<< HEAD
+# <<<<<<< HEAD
   #ap English
   
   
-=======
->>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
+# =======
+# >>>>>>> 74f21996e13f02fefcdb3fc8635072396ee28d5a
 
 
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
